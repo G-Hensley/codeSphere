1c36fd3677cfd2551d9fc365593e079ea095300a
Christin-paige <christinpmartin@gmail.com>
2025-04-13 10:38:30 -0400

    added routing and about content

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+export default function Dashboard (){
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-[#1d1d1d] to-[#86059F]">
+            <h1 className="text-4xl font-bold text-white">Dashboard</h1>
+            <p className="mt-4 text-lg text-white">Welcome to your dashboard!</p>
+        </div>
+    )
+}
